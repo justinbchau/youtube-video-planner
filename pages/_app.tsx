9e68ee45cddab1e3931e84cb7890ad158edfd855
Justin Chau <justinchau@Js-Macbook.local>
2022-01-12 23:15:31 -0800

    changed api path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
-    uri: 'http://localhost:3000/api/graphql',
+    uri: '/api/graphql',
     cache: new InMemoryCache()
   });
 
